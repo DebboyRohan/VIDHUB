60a02cc9235412c22f63004313deb5d6a6336710
rohan <rohankumarpandey234@gmail.com>
2024-12-07 20:18:24 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+
+const app = express()
+//app.use() -> is used for middlewares
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//Now these app.use() will do file type handling
+app.use(express.json({
+    limit : "16kb"
+}));
+app.use(express.urlencoded({
+    extended: true,
+    limit : "16kb"
+}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app }
